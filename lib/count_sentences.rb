0909530754486777d7608d1c1e4d584283a6b989
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-31 01:11:00 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else 
+      false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false 
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else 
+      false
+    end 
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
